18c8ec324374a4a30ff6323de0d8d6205708190e
Nicole Zheng <zznicole@hotmail.com>
2020-06-28 00:05:44 +0200

    fetch data

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,17 @@
   <div id="app">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search..."/>
+        <input 
+        type="text" 
+        class="search-bar" 
+        placeholder="Search..."
+        v-model="query"
+        @keypress="fetchWeather"
+        />
       </div>
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Linköping, Sweden</div>
+          <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
           <div class="date">Saturday 27 June 2020</div>
         </div>
         <div class="weather-box">
@@ -25,8 +31,25 @@ export default {
   name: 'App',
   data() {
     return {
-      api_key: '10e1701456eedef1375212b9177af403'
+      api_key: '10e1701456eedef1375212b9177af403',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+
     }
+  },
+  methods: {
+    fetchWeather (e) {
+      if (e.key == 'Enter'){
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => res.json())
+          .then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+    }
+
   }
 
 }
@@ -44,7 +67,7 @@ body {
 }
 
 #app {
-  background-image: url('./assets/cold-bg.png');
+  background-image: url('./assets/warm-bg.png');
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
