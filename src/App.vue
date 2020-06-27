c8760ca518d09c88adfcdbd4730a686cd1cc6d82
Nicole Zheng <zznicole@hotmail.com>
2020-06-28 00:41:36 +0200

    create dateBuilder

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
-          <div class="date">Saturday 27 June 2020</div>
+          <div class="date">{{dateBuilder()}}</div>
         </div>
         <div class="weather-box">
           <div class="temp">{{Math.round(weather.main.temp)}}°</div>
@@ -48,6 +48,18 @@ export default {
     },
     setResults (results) {
       this.weather = results;
+    },
+    dateBuilder () {
+      let d = new Date()
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
     }
 
   }
